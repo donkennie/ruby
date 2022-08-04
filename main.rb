cd805f54e9a6c96e53f4b603a020690e72a4243a
donkennie <ajeigbekehinde160@gmail.com>
2022-08-04 04:30:23 +0100

    Initial commit

---
+++ main.rb
@@ -0,0 +1,90 @@
+# load 'hello.rb'
+
+# puts "Hello world"
+# p "Hello"
+# print "Hello Again"
+
+# single line comment
+
+=begin
+okay
+yeah
+true
+=end
+
+# name = "Kenny"
+# puts name
+
+# num =123
+# cond = true
+
+# puts name, num , cond
+
+# print "Hello #{name}"
+
+# print "Enter your name : "
+# name = gets.chomp
+
+# print "Enter your ID : "
+# id = gets.chomp.to_i
+# puts "Hello #{name} Your id is #{id}"
+
+# str1 = "This is a string"
+# str2 = "This is another string"
+
+# puts str1
+# puts str2
+
+# str3 = "This is a
+# multiline
+# string"
+
+# puts str3
+
+
+# str4 = %/This 
+# is multi
+# line/
+
+# puts str4
+
+# str5 = "Hello"
+# puts str5.size
+# puts str5.length
+
+# puts str5.upcase
+# puts str5.downcase
+# puts str5.reverse
+# puts str5.upcase.reverse 
+# puts str5.include? "Hello";
+
+# puts "Hello " + "How are you";
+
+# puts "Hello ".concat("How are you...");
+
+
+str6 = "Hi"
+puts str6
+#str6 = str6 << "again" #append
+
+puts str6
+
+#str6.freeze #unable to modify
+str6 = str6 << "again"
+
+puts "abc" == "abc"
+puts "abc".eql? "123"
+
+
+msg = "Hello from Automation step by step"
+puts msg
+
+puts msg["Hello"]
+
+puts msg[0]
+puts msg[0, 4]
+puts msg[0, 20]
+puts msg[0, msg.length]
+puts msg[-4]
+puts msg.length
+
