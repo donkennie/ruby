1439f8230f31b5f01e15385b6c67acfe91949a58
donkennie <ajeigbekehinde160@gmail.com>
2022-08-09 15:22:39 +0100

    loop syntax

---
+++ loop.rb
@@ -0,0 +1,34 @@
+#While loop demo
+#Display a message 5 times
+#Initialize a loop variable
+
+
+# count = 0
+# #while loop begins
+
+# while ( count < 5 ) do
+
+# puts "\nInside while loop!"
+
+# #Increment loop variable
+
+# count = count + 1
+
+# end
+
+
+#Until loop demo
+#Display multiples of 3
+#Initialize a loop variable
+
+count = 1
+
+#until loop begins
+until ( count == 11 ) do
+
+puts "\n #{count * 3}"
+
+#Increment loop variable
+count = count + 1
+
+end
