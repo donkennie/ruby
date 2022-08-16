e194fe88d40cdcead051e76b4dd88f606c9e8fcb
donkennie <ajeigbekehinde160@gmail.com>
2022-08-16 19:07:00 +0100

    Understand ruby if/else statement syntax

---
+++ statement.rb
@@ -0,0 +1,28 @@
+# condition = true
+# another_condition = false
+
+# if condition || another_condition #or !true
+
+#     puts "this evaluated to true"
+
+# else
+#     puts "this evaluated to false"
+
+# end
+
+# puts "La la la"
+
+name = "Kenny"
+
+if name == "kenny"
+
+    puts "Welcome back"
+
+elsif name == "Ken"
+
+    puts "Good night"
+
+else
+
+    puts "Good bye"
+end
